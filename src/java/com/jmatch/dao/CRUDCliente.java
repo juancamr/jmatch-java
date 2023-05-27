b3b5afbac4107edc07f4478eaafec0b139e9db4b
Juan Carlos <jcmrojas29@gmail.com>
2023-05-27 14:34:16 -0500

    refactor

--- src/java/com/jmatch/dao/CRUDCliente.java
+++ src/java/com/jmatch/dao/CRUDCliente.java
@@ -1,25 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.jmatch.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import com.jmatch.interfaces.ClienteInterface;
 import com.jmatch.modelo.Cliente;
 
-/**
- *
- * @author JOSE HUAVIL
- */
 public class CRUDCliente implements ClienteInterface{
     
     DBConnection cn=new DBConnection();
--- src/java/com/jmatch/dao/DBConnection.java
+++ src/java/com/jmatch/dao/DBConnection.java
@@ -1,20 +1,20 @@
-
 package com.jmatch.dao;
 
 import com.jmatch.interfaces.DbCredentials;
 import java.sql.*;
 
 public class DBConnection implements DbCredentials {
+
     String port = PORT;
     String dbname = DBNAME;
     String user = USER;
     String passwd = PASSWD;
-    
+
     public static Connection connect;
     public Statement st;
     public PreparedStatement ps;
     public ResultSet rs;
-    
+
     public DBConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -26,9 +26,15 @@ public class DBConnection implements DbCredentials {
             System.out.println(e);
         }
     }
-    
+
     public static void closeConnection() throws SQLException {
         connect.close();
     }
-    
+
+    public static String statusConnection() throws SQLException {
+        return (connect != null && !connect.isClosed())
+                ? "Conexion establecida"
+                : "Error en la conexion";
+    }
+
 }
